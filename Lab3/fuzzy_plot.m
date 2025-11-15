67ecd8c2dce8761a0954b5e1735fb2be9a9857a0
DESKTOP-C3T458L\vanchik21 <ipz221_zia@student.ztu.edu.ua>
2025-11-16 00:32:30 +0200

    lab3 comleted

---
+++ Lab3/fuzzy_plot.m
@@ -0,0 +1,16 @@
+% Побудова графіка функції y=(x1^2-8)*cos(x2)
+% в області x1∈[0,4] і x2∈[0,4]
+n = 15;
+x1 = 0:4/(n-1):4;
+x2 = 0:4/(n-1):4;
+y = zeros(n,n);
+
+for j = 1:n
+    y(j,:) = (x1.^2 - 8) * cos(x2(j));
+end
+
+surf(x1, x2, y)
+xlabel('x1')
+ylabel('x2')
+zlabel('y')
+title('Target')
